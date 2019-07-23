bbbc652f12dfc71f889f40c3831cd502e20c2217
Lenikas <stuffer232@gmail.com>
2019-07-23 19:52:13 +0500

    загрузка страницы и начал поиск ссылок

--- save_page.py
+++ save_page.py
@@ -1,37 +1,43 @@
 import urllib.request
 import sys
 from bs4 import BeautifulSoup
-import urllib
+from urllib.request import Request, urlopen
+import re
 
 
 def save_page(url):
-    html = urllib.request.urlopen(url).read()
-    f = open('index.html', 'w')
-    f.write(str(html))
-    f.close()
+    """
+    Скачивает html страницы и записывает в файл с именем (после http://)
+    """
+    req = Request(
+        url, headers={'User-Agent': 'Mozilla/5.0'})  # чтобы обойти блокировку
+    html = urlopen(req).read()
+    with open('{0}'.format(re.split(r'/', url)[2] + '.html'), "w") as file:
+        file.write(str(html))
+    return html
 
+
+def get_links(html):
+    soup = BeautifulSoup(html)
+    links = soup.findAll('a', attrs={'href': re.compile("^http://")})
+    # links = soup.findAll('a') находит еще и href
+    only_http = re.findall(r'https?://\S+', str(links[0]))
+    print(only_http)
+    return only_http
 
 def recursive_url(url, depth):
     if depth == 5:
         return url
     else:
-        page = urllib.request.urlopen(url)
-        soup = BeautifulSoup(page.read())
-        newlink = soup.find('a') #  шаблон для ссылок сделать
+        page=urllib.request.urlopen(url)
+        soup=BeautifulSoup(page.read())
+        newlink=soup.find('a')
         if len(newlink) == 0:
             return url
         else:
             return url, recursive_url(newlink, depth+1)
 
 
-def get_links(url):
-    page = urllib.request.urlopen(url)
-    soup = (page.read())
-    links = soup.find_all('a', {'class': 'institution'})
-    for link in links:
-        links.append(recursive_url(link, 0))
-    return links
-
-
 if __name__ == "__main__":
-    save_page(sys.argv[1])
+    page=save_page(sys.argv[1])
+    get_links(page)
