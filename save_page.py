c7e73c78ad5e0af53ec29d975641d67a8044d44d
Lenikas <stuffer232@gmail.com>
2019-08-18 20:16:37 +0500

    сделал нейминг файлов и загрузку

--- save_page.py
+++ save_page.py
@@ -7,12 +7,19 @@ import re
 from urllib.request import urlopen
 
 
+def get_name(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    names = [name.text_content().strip() for name in tree.cssselect('title')]
+    return names
+
+
 def save_page(url):
     #html = urlopen(url).read()
     page = requests.get(url)
     data = page.text
     soup = BeautifulSoup(data).encode('utf-8')
-    name_page = "{0}.html".format(url[7:9]).replace("/", "_")
+    name_page = "{0}.html".format(get_name(url))
     with open(name_page, "wb") as page:
         page.write(soup)
 
@@ -28,26 +35,27 @@ def get_links(soup):
     list_links = []
     for link in soup.find_all(attrs={'href': re.compile("http")}):
         list_links.append(link.get('href'))
-    print(list_links)
     return list_links
 
 
-def recursive_find_url(list_links, max_depth=1):
+def recursive_find_url(list_links, max_depth=2):
     if max_depth > 0:
         for link in list_links:
+            save_page(link)
+            print(link)
             soup = get_soup(link)
-            get_links(soup)
-            max_depth = max_depth - 1
+            links = get_links(soup)
+            recursive_find_url(links, max_depth=max_depth-1)
 
 
 def main():
     url = sys.argv[1]
+    save_page(url)
     #max_depth = sys.argv[2]
     soup = get_soup(url)
     list_links = get_links(soup)
     recursive_find_url(list_links)
+    
    
-
-
 if __name__ == '__main__':
     main()
