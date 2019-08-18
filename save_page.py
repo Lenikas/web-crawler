f9fab325e2c5cbcd58eb0a9c36ba2ed10317e504
Lenikas <stuffer232@gmail.com>
2019-08-18 15:43:16 +0500

    поиск ссылок есть, нужно разобраться с загрузкой страниц и неймингом

--- save_page.py
+++ save_page.py
@@ -3,31 +3,51 @@ import requests
 import lxml
 from lxml import html
 import sys
+import re
+from urllib.request import urlopen
 
 
-def find_links(url):
-    """
-    Находит все ссылки на указанной странице и возвращает список из этих ссылок
-    """
-    #   main_url = "https://lecturesnet.readthedocs.io/net/web.scraping/lxml.html"
-    re = requests.get(url)
-    root = lxml.html.fromstring(re.content)
-    page_links = [url + link for link in root.xpath('//a/@href')]
-    print(page_links)
-    return page_links
+def save_page(url):
+    #html = urlopen(url).read()
+    page = requests.get(url)
+    data = page.text
+    soup = BeautifulSoup(data).encode('utf-8')
+    name_page = "{0}.html".format(url[7:9]).replace("/", "_")
+    with open(name_page, "wb") as page:
+        page.write(soup)
 
 
-def recursive_find_url(list_links):
-    for link in list_links:
-        find_links(link)
+def get_soup(url):
+    page = requests.get(url)    
+    data = page.text
+    soup = BeautifulSoup(data)
+    return soup
+
+
+def get_links(soup):
+    list_links = []
+    for link in soup.find_all(attrs={'href': re.compile("http")}):
+        list_links.append(link.get('href'))
+    print(list_links)
+    return list_links
+
+
+def recursive_find_url(list_links, max_depth=1):
+    if max_depth > 0:
+        for link in list_links:
+            soup = get_soup(link)
+            get_links(soup)
+            max_depth = max_depth - 1
 
 
 def main():
     url = sys.argv[1]
-    list_links = find_links(url)
+    #max_depth = sys.argv[2]
+    soup = get_soup(url)
+    list_links = get_links(soup)
     recursive_find_url(list_links)
+   
 
 
 if __name__ == '__main__':
     main()
-
